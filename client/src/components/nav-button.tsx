932f4d7901210aec2fca466923c45468507b797d
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-08-04 20:49:00 +0200

    4. Navbar, svg icon

---
+++ client/src/components/nav-button.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from "react";
+import { Button } from "./ui/button";
+
+interface NavButtonProps {
+  children?: React.ReactNode;
+}
+
+const NavButton = forwardRef<HTMLButtonElement, NavButtonProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <Button
+        ref={ref}
+        className="rounded-3xl bg-transparent p-4 font-bold hover:bg-white/10 hover:transition-all focus:bg-white/10 active:bg-white/20"
+        {...props}
+      >
+        {children}
+      </Button>
+    );
+  },
+);
+
+NavButton.displayName = "NavButton"; // Good practice for debugging
+
+export default NavButton;
---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,65 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import NavButton from "./nav-button";
+import { ChevronDownIcon, CircleUserRoundIcon } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <div className="flex h-14 w-full items-center justify-between bg-gray-900 px-4">
+      <div className="container mx-auto flex max-w-7xl items-center justify-between">
+        <div className="flex h-8 w-16 items-center justify-center rounded-sm bg-amber-300">
+          <p
+            className="font-impact text-lg font-normal tracking-wide text-black"
+            style={{
+              transform: "scaleY(1.25) scaleX(1.25)",
+              alignContent: "center",
+            }}
+          >
+            IMXd
+          </p>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <NavButton>
+              <CircleUserRoundIcon className="size-6" />
+              Username
+              <ChevronDownIcon className="size-4" />
+            </NavButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="font-roboto h-full w-45 rounded-xs border-0 bg-gray-800 p-0 py-2 font-normal text-white!"
+            align="end"
+          >
+            <DropdownMenuItem className="rounded-x py-3 pl-4 text-base hover:bg-white/10! hover:text-white!">
+              Your Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem className="py-2.5 pl-4 text-base hover:bg-white/15! hover:text-white!">
+              Your Watchlist
+            </DropdownMenuItem>
+            <DropdownMenuItem className="py-2.5 pl-4 text-base hover:bg-white/15! hover:text-white!">
+              Your Ratings
+            </DropdownMenuItem>
+            <DropdownMenuItem className="py-2.5 pl-4 text-base hover:bg-white/15! hover:text-white!">
+              Your Lists
+            </DropdownMenuItem>
+            <DropdownMenuItem className="py-2.5 pl-4 text-base hover:bg-white/15! hover:text-white!">
+              Your Watch History
+            </DropdownMenuItem>
+            <DropdownMenuItem className="py-2.5 pl-4 text-base hover:bg-white/15! hover:text-white!">
+              Account Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem className="py-2.5 pl-4 text-base hover:bg-white/15! hover:text-white!">
+              Sign out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
