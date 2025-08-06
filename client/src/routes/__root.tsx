11a3620b54461856fd898b894795ae7ec8e956c1
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-08-06 12:45:51 +0200

    5. Tanstack router setup

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/navbar";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <div className="text-foreground bg-custom-white flex min-h-screen w-full flex-col">
+        <Navbar />
+        <main className="container mx-auto grow p-4">
+          {/* This is where the nested routes will be rendered */}
+          <Outlet />
+        </main>
+      </div>
+
+      {/* Tanstack router stuff */}
+      {/* Devtools for debugging the router */}
+      <TanStackRouterDevtools position="bottom-left" />
+    </>
+  );
+}
